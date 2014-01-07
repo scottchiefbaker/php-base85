2bfc3935d02d351f427ddd07125ee02ead24d49f
Scott Baker <scott@perturb.org>
2014-01-07 14:27:01 -0800

    Remove everything except encode() and decode()

--- base85.php
+++ base85.php
@@ -1,27 +1,7 @@
 <?PHP
 
-#base85::run_tests();
-#print base85::encode("Hello World");    // Should be: 87cURD]i,"Ebo7
-#print base85::decode('87cURD]i,"Ebo7'); // Should be: Hello World
-
-/////////////////////////////////////////////////////////////////
-
 class base85 
 {
-
-	static function run_tests() {
-		base85::my_test('The quick brown fow jumps over the lazy dog.','<+ohcEHPu*CER),Dg-(AAoDl9C3=B4F!,CEATAo8BOr<&@=!2AA8c*5');
-		base85::my_test("Hello world",'87cURD]j7BEbo7');
-
-		base85::my_test('D9uWjh[','6ofBkC1pf');
-		base85::my_test('JCINaFj]b','8jc0F@7G!;@K');
-		base85::my_test('knQKwu','CMm!BGBE');
-		base85::my_test('F9IXlmG','7QF%BCi)Z');
-
-		// Raw non-printable data
-		$bytes = base64_decode("Ho4q/TxtN3xiKfBafvdtkFAYAZ4=");
-		base85::my_test($bytes,'*f_`K4Dd$)@O^eMIeR]@:`\'5)');
-	}
 
 	public static function decode($str) {
 		$str = preg_replace("/ \t\r\n\f/","",$str);
@@ -114,51 +94,4 @@ class base85
 		return $ret;
 	}
 
-	private static function my_test($str,$result) {
-		$enc     = base85::encode($str);
-		$correct = intval($result == $enc);
-		$raw_str = $str;
-
-		$enc = htmlentities($enc);
-		$str = htmlentities($str);
-
-		if ($correct) {
-			$color = "lightgreen";
-		} else {
-			$color = "red";
-		}
-
-		if (!ctype_print($str)) {
-			$str = "<code><b>" . md5($str) . "</b></code>";
-		} else {
-			$str = "\"$str\"";
-		}
-
-		print "<div>Testing: $str</div>\n";
-		print "<span style=\"background-color: $color\">Encode: $str => \"$enc\"</span>\n";
-		print "<br />";
-
-		$decoded = base85::decode($result);
-		$correct = intval($decoded == $raw_str);
-
-		if ($correct) {
-			$color = "lightgreen";
-		} else {
-			$color = "red";
-		}
-
-		$result  = htmlentities($result);
-		$decoded = htmlentities($decoded);
-
-		if (!ctype_print($decoded)) {
-			$decoded = "<code><b>" . md5($decoded) . "</b></code>";
-		} else {
-			$decoded = "\"$decoded\"";
-		}
-
-		print "<span style=\"background-color: $color\">Decode: \"$result\" => $decoded</span>\n";
-		print "<br />";
-		print "<br />";
-	}
-
-}
+} // End of base85 class
