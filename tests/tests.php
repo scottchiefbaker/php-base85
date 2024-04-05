ce7c02f70de6911e44ba1518e6594a9c96b0c183
Scott Baker <scott@perturb.org>
2024-04-05 11:54:32 -0700

    Add summary to the tests at the end

--- tests/tests.php
+++ tests/tests.php
@@ -3,32 +3,50 @@
 $file = __DIR__ . "/../base85.class.php";
 require($file);
 
-$ok = 0;
+$pass_count = 0;
+$fail_count = 0;
 
-$ok += is_equal(base85::encode("\0"),'!!', "Null");
-$ok += is_equal(base85::encode('    '),'y', "Four spaces = 'y'");
-$ok += is_equal(base85::encode(' '),'+9', "Single space");
-$ok += is_equal(base85::encode(str_repeat("\0",4)),'z', 'Four nulls');
+is_equal(base85::encode("\0"),'!!', "Null");
+is_equal(base85::encode('    '),'y', "Four spaces = 'y'");
+is_equal(base85::encode(' '),'+9', "Single space");
+is_equal(base85::encode(str_repeat("\0",4)),'z', 'Four nulls');
 
-$ok += is_equal(base85::encode("food\0bat"),'AoDTu!+KAY', "Null in middle");
-$ok += is_equal(base85::encode("bird\0\0\0\0bath"),'@VKjnz@UX@l', 'Four null in middle');
-$ok += is_equal(base85::encode('The quick brown fox jumps over the lazy dog.'),'<+ohcEHPu*CER),Dg-(AAoDo:C3=B4F!,CEATAo8BOr<&@=!2AA8c*5', "Quick brown fox");
-$ok += is_equal(base85::encode("Hello world"),'87cURD]j7BEbo7', "Hello world");
+is_equal(base85::encode("food\0bat"),'AoDTu!+KAY', "Null in middle");
+is_equal(base85::encode("bird\0\0\0\0bath"),'@VKjnz@UX@l', 'Four null in middle');
+is_equal(base85::encode('The quick brown fox jumps over the lazy dog.'),'<+ohcEHPu*CER),Dg-(AAoDo:C3=B4F!,CEATAo8BOr<&@=!2AA8c*5', "Quick brown fox");
+is_equal(base85::encode("Hello world"),'87cURD]j7BEbo7', "Hello world");
 
-$ok += is_equal(base85::encode('D9uWjh['),'6ofBkC1pf', "String #1");
-$ok += is_equal(base85::encode('JCINaFj]b'),'8jc0F@7G!;@K', "String #2");
-$ok += is_equal(base85::encode('knQKwu'),'CMm!BGBE', "String #3");
-$ok += is_equal(base85::encode('F9IXlmG'),'7QF%BCi)Z', "String #4");
+is_equal(base85::encode('D9uWjh['),'6ofBkC1pf', "String #1");
+is_equal(base85::encode('JCINaFj]b'),'8jc0F@7G!;@K', "String #2");
+is_equal(base85::encode('knQKwu'),'CMm!BGBE', "String #3");
+is_equal(base85::encode('F9IXlmG'),'7QF%BCi)Z', "String #4");
 
 // Raw non-printable data
 $bytes  = base64_decode("Ho4q/TxtN3xiKfBafvdtkFAYAZ4=");
-$ok    += is_equal(base85::encode($bytes),'*f_`K4Dd$)@O^eMIeR]@:`\'5)', "Unprintable chars");
+is_equal(base85::encode($bytes),'*f_`K4Dd$)@O^eMIeR]@:`\'5)', "Unprintable chars");
 
-$ok += is_equal(base85::encode(md5("test", true)),'$\'/lH7Np6%b2,mG-7?1o', "Github issue #2");
+// Github issues
+is_equal(base85::encode(md5("test", true)),'$\'/lH7Np6%b2,mG-7?1o', "Github issue #2");
+
+print "\n";
+
+$green = "\033[38;5;10m";
+$red   = "\033[38;5;9m";
+$reset = "\033[0m";
+
+if ($fail_count) {
+	print $red . "$fail_count tests failed\n" . $reset;
+	exit(7);
+} else {
+	print $green . "All $pass_count tests passed\n" . $reset;
+}
 
 ////////////////////////////////////////////////////////////////////
 
 function is_equal($input, $expected, $name = "") {
+	global $pass_count;
+	global $fail_count;
+
 	if ($name) {
 		$test_name = $name;
 	} else {
@@ -50,8 +68,12 @@ function is_equal($input, $expected, $name = "") {
 
 	if ($input === $expected) {
 		print $lead . $pad . $ok_str . "\n";
+
+		$pass_count++;
 	} else {
 		print $lead . $pad . $fail_str . "\n";
 		print "  * Expected '$expected' got '$input'\n";
+
+		$fail_count++;
 	}
 }
